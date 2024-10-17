b35c7ecbdb80d1ea60beffa22b59d1ce9f3b8d17
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-17 22:01:17 +0530

    Create PrintingStringAndIntegers.java

---
+++ Basics/PrintingStringAndIntegers.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class PrintingStringAndIntegers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 10;
+		int z = 15;
+		System.out.println("String"+x+y+z);
+		System.out.println(x+"String"+y+z);
+		System.out.println(x+y+"String"+z);
+
+	}
+
+}
