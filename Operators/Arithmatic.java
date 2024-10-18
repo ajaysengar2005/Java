9c02fb4b2252736e8a5f9f004c04ea5f8bb54f27
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-18 22:18:06 +0530

    Create Arithmatic.java

---
+++ Operators/Arithmatic.java
@@ -0,0 +1,17 @@
+package operators;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 10;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+
+	}
+
+}
