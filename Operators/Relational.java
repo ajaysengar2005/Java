beaa7a65d1c7dfb50523ba6ced7d05c28c06e686
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-18 22:18:36 +0530

    Create Relational.java

---
+++ Operators/Relational.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 3;
+		System.out.println(x==y);
+		System.out.println(x!=y);
+		System.out.println(x<y);
+		System.out.println(x>y);
+		System.out.println(x<=y);
+		System.out.println(x>=y);
+
+	}
+
+}
