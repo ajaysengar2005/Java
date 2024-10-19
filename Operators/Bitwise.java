c34d0c27dc51a1ced0cbb17805e2def1a5a56c33
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-19 23:40:44 +0530

    Create Bitwise.java

---
+++ Operators/Bitwise.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 2;
+		System.out.println(x&y);
+		System.out.println(x|y);
+		System.out.println(~x);
+		System.out.println(x^y);
+		System.out.println(x<<1);
+		System.out.println(x>>1);
+
+	}
+
+}
