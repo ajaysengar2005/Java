4f8decdd495eacb0de6fa027ebae055accd6cd69
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-20 22:43:06 +0530

    Create Unary.java

---
+++ Operators/Unary.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class UnaryOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		System.out.println(x++);
+		System.out.println(x);
+		
+		System.out.println(++x);
+		System.out.println(x);
+		
+		x--;
+		System.out.println(x);
+		
+		--x;
+		System.out.println(x);
+
+	}
+
+}
+
