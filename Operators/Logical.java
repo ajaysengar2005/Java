1eccde5c1b28e2192f0fb5f80919250c93abc20f
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-20 22:42:26 +0530

    Create Logical.java

---
+++ Operators/Logical.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 6;
+		int y = 8;
+		System.out.println(x%2==0 && y%2==0);
+		System.out.println(x%2==0 || y%2==0);
+		System.out.println(!(x%2==0));
+
+	}
+
+}
