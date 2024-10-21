0e3c808e6fb77046cd2e03563210b08592d79907
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-21 21:45:09 +0530

    Create SimpleInterest.java

---
+++ Practice/SimpleInterest.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class CalculateSimpleInterest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double principle = 900;
+		double rate = 3.4;
+		double time = 2;
+		
+		double si = (principle*rate*time)/100;
+		
+		System.out.println("Simple Interest : "+si);
+
+	}
+
+}
