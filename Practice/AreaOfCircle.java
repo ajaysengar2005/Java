a25024e04ca182a644e7d00e78137f1702697cf6
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-21 21:44:33 +0530

    Create AreaOfCircle.java

---
+++ Practice/AreaOfCircle.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class CalculateAreaOfCircile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 10;
+		double pi = 3.14;
+		
+		double area = pi*radius*radius;
+		
+		System.out.println(area);
+
+	}
+
+}
