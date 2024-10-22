bf9e69304695656092860adbfe16b586f06f1b10
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-22 20:51:15 +0530

    Create CalculateSum.java

---
+++ Practice/CalculateSum.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class CalculateSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 4;
+		int b = 5;
+		System.out.println("Sum is : "+(a+b));
+
+	}
+
+}
