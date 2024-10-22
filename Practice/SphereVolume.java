dc46a53f11c51c99324a13a4a4b4b7401506114c
Ajay Pratap Singh Sengar <sengarajay0987@gmail.com>
2024-10-22 20:52:01 +0530

    Create SphereVolume.java

---
+++ Practice/SphereVolume.java
@@ -0,0 +1,17 @@
+package practice;
+
+public class CalculateVolumeOfSphere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double radius = 1;
+		double pi = 3.14159265359;
+		
+		
+		double volume = (4.0/3.0)*(pi*radius*radius*radius);
+		
+		System.out.println(volume);
+
+	}
+
+}
